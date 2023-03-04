3a688de4adced3c66613c21e30d6045268380e0e
emmanuel <aderanti111@gmail.com>
2023-03-04 17:56:15 +0100

    first commit

---
+++ assignment.kt
@@ -0,0 +1,7 @@
+import kotlin.math.PI
+
+fun main(args: Array<String>) {
+    var radius = readln()!!.toDouble
+    var area_of_circle  = PI * radius * radius
+    println("The area of circle with radius $radius is $area_of_circle")
+    }
